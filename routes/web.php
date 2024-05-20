0be9ecbc44d53905b5cbb8b32fccbcd817e4d5cd
Yefta Steven Marcellius <yeftasm@gmail.com>
2024-05-20 09:08:08 +0700

    update login form

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+Route::get('/admin/items/index', [App\Http\Controllers\ItemController::class, 'index'])->name('items.index');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin/category/index', [App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
+Route::post('/admin/category/store', [App\Http\Controllers\CategoryController::class, 'store'])->name('category.store');
+
+
